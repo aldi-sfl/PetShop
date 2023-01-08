e40773d1e1c3abf541b3deebf902f7a12001868b
aprimanfikri <aprmnfkr@gmail.com>
2023-01-08 23:41:09 +0700

    Mencoba membuat login register menggunakan username

---
+++ PetShop/cobacoba.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PetShop
+{
+    partial class cobacoba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtconfirmpassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnregister = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtconfirmpassword
+            // 
+            this.txtconfirmpassword.Location = new System.Drawing.Point(268, 197);
+            this.txtconfirmpassword.Name = "txtconfirmpassword";
+            this.txtconfirmpassword.Size = new System.Drawing.Size(397, 27);
+            this.txtconfirmpassword.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 29);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Confirm Password";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnlogin.Location = new System.Drawing.Point(548, 267);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(117, 53);
+            this.btnlogin.TabIndex = 15;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnregister
+            // 
+            this.btnregister.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnregister.Location = new System.Drawing.Point(12, 267);
+            this.btnregister.Name = "btnregister";
+            this.btnregister.Size = new System.Drawing.Size(117, 53);
+            this.btnregister.TabIndex = 16;
+            this.btnregister.Text = "Register";
+            this.btnregister.UseVisualStyleBackColor = true;
+            this.btnregister.Click += new System.EventHandler(this.btnregister_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(268, 164);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(397, 27);
+            this.txtpassword.TabIndex = 13;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(268, 131);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(397, 27);
+            this.txtusername.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Username";
+            // 
+            // cobacoba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 450);
+            this.Controls.Add(this.txtconfirmpassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.btnregister);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Name = "cobacoba";
+            this.Text = "cobacoba";
+            this.Load += new System.EventHandler(this.cobacoba_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtconfirmpassword;
+        private Label label1;
+        private Button btnlogin;
+        private Button btnregister;
+        private TextBox txtpassword;
+        private TextBox txtusername;
+        private Label label6;
+        private Label label5;
+    }
+}
